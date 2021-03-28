8b8c8329a0988f525b15062eded3ca4b4734de6b
mariobaeta <81488632+mariobaeta@users.noreply.github.com>
2021-03-28 17:05:20 +0100

    Criada pasta Funçoes
    aprendendo funções em py

---
+++ Funções.py
@@ -0,0 +1,11 @@
+# Função print()
+
+nome= input ("Digite o seu nome: ")
+print("Olá" , nome)
+
+# Isto é um comentário e não será executado pelo Python
+print("Oi Python")
+
+# isto também é um comentário e não será executado
+
+print("Explicando comentários") 
